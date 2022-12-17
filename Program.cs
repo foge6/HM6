793c570dd7bf9f11a912344a26482be62ea30ff1
makar <makar.t1@mail.ru>
2022-12-17 14:23:53 +0300

    hm6

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// void Q41 () 
+// {int count=0; 
+// int M = Convert.ToInt32(Console.ReadLine()); 
+// for (int i = 0; i < M; i++)
+//     {int numb = Convert.ToInt32(Console.ReadLine());
+//     if (numb>0)
+//     count++;}   
+//     System.Console.WriteLine(count);}
+// Q41();
+
+// void Find (double k1, double b1, double k2, double b2)
+// {
+//     double x = (b2 - b1)/(k1 - k2);
+//     System.Console.WriteLine(x);
+//     double y = k1*x + b1;
+//     System.Console.WriteLine($"{x};{y}");
+// }
+// System.Console.WriteLine("Write k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Write b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Write k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Write b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Find(k1,b1,k2,b2);
