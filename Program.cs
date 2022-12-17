066786fc9c013b43fe048b5ac1719919eb476693
foge6 <118968994+foge6@users.noreply.github.com>
2022-12-17 14:29:04 +0300

    Update Program.cs
    del show x

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@
 // void Find (double k1, double b1, double k2, double b2)
 // {
 //     double x = (b2 - b1)/(k1 - k2);
-//     System.Console.WriteLine(x);
 //     double y = k1*x + b1;
 //     System.Console.WriteLine($"{x};{y}");
 // }
